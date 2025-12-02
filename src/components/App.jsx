6c22f10432bf3e54fa7697d751273597d19ab24c
Esther Quirós <estherbequiros@gmail.com>
2025-12-02 16:22:07 +0100

    add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,12 @@ function App() {
       <header className="header">
         <h1 className="title">Plantilla React</h1>
       </header>
-      <main className="main">Holis Adalabers!!!</main>
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<h1>Home page</h1>} />
+          <Route path="character/:id" element={<h1>Details page</h1>} />
+        </Routes>
+      </main>
     </div>
   );
 }

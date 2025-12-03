e7b0942e415de1d5f42fc570ecdaf5c74b818cb0
Esther Quirós <estherbequiros@gmail.com>
2025-12-03 18:20:33 +0100

    add details page complete

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,14 +1,18 @@
+import { Link } from "react-router";
+
 function CharacterCard({ character }) {
   return (
-    <li key={character.id} className="character-card">
-      <h3 className="character-card__name">{character.name}</h3>
-      <p className="character-card__species">{character.especies}</p>
-      <img
-        className="character-card__img"
-        src={character.image || "images/avatar-placeholder.png"}
-        alt={`Imagen de ${character.name}`}
-      />
-    </li>
+    <Link to={`/character/${character.id}`}>
+      <li key={character.id} className="character-card">
+        <h3 className="character-card__name">{character.name}</h3>
+        <p className="character-card__species">{character.especies}</p>
+        <img
+          className="character-card__img"
+          src={character.image || "images/avatar-placeholder.png"}
+          alt={`Imagen de ${character.name}`}
+        />
+      </li>
+    </Link>
   );
 }
 export default CharacterCard;
--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,16 +1,38 @@
 import { useParams } from "react-router";
 import { useState } from "react";
+import { useEffect } from "react";
 
 function CharacterDetail({ characters }) {
-  const characterId = useParams("id");
+  const params = useParams("id");
 
   const [character, setCharacter] = useState({
     name: "Cargando...",
   });
 
+  useEffect(() => {
+    if (characters.length === 0) return;
+
+    const character = characters.find((c) => {
+      return c.id === params.id;
+    });
+
+    setCharacter(character);
+  }, [characters, params.id]);
+
   return (
     <section className="character-detail">
+      <img
+        className="character-detail__img"
+        src={character.image || "images/avatar-placeholder.png"}
+        alt={`Imagen de ${character.name}`}
+      />
       <h2 className="character-detail__name">{character.name}</h2>
+      <p className="character-detail__alive">
+        {character.alive ? "Alive" : "Dead"}
+      </p>
+      <p className="character-detail__species">{character.species}</p>
+      <p className="character-detail__gender">{character.gender}</p>
+      <p className="character-detail__house">{character.house}</p>
     </section>
   );
 }

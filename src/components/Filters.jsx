1b68a172a5c49d2137d094283e1cab9aefd00d98
Esther Quirós <estherbequiros@gmail.com>
2025-12-04 12:38:36 +0100

    add styles

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -7,29 +7,33 @@ function Filters({
   return (
     <section className="filters">
       <form className="filters__form" onSubmit={(ev) => ev.preventDefault()}>
-        <label className="filter__name-label" htmlFor="name">
-          Buscar🔍
-        </label>
-        <input
-          className="filters__name-input"
-          name="name"
-          type="text"
-          onInput={handleInput}
-        />
-        <label className="filters__houses-label" htmlFor="houses"></label>
-        <select
-          className="filters__houses-select"
-          name="houses"
-          id="houses"
-          onChange={handleChange}
-        >
-          <option value={""}>Todas las casas</option>
-          <option value={"Gryffindor"}>🦁 Gryffindor</option>
-          <option value={"Slytherin"}>🐍 Slytherin</option>
-          <option value={"Hufflepuff"}>🦡 Hufflepuff</option>
-          <option value={"Ravenclaw"}> 🦅Ravenclaw</option>
-        </select>
-        <div>
+        <div className="filters__name">
+          <label className="filter__name-label" htmlFor="name">
+            Buscar🔍
+          </label>
+          <input
+            className="filters__name-input"
+            name="name"
+            type="text"
+            onInput={handleInput}
+          />
+        </div>
+        <div className="filters__houses">
+          <label className="filters__houses-label" htmlFor="houses"></label>
+          <select
+            className="filters__houses-select"
+            name="houses"
+            id="houses"
+            onChange={handleChange}
+          >
+            <option value={""}>Todas las casas</option>
+            <option value={"Gryffindor"}>🦁 Gryffindor</option>
+            <option value={"Slytherin"}>🐍 Slytherin</option>
+            <option value={"Hufflepuff"}>🦡 Hufflepuff</option>
+            <option value={"Ravenclaw"}> 🦅Ravenclaw</option>
+          </select>
+        </div>
+        <div className="filters__gender">
           <input
             type="radio"
             id="all"
@@ -55,7 +59,7 @@ function Filters({
           />
           <label for="male">Hombre</label>
         </div>
-        <div>
+        <div className="filters__alphabetic">
           <input
             type="checkbox"
             id="all"
